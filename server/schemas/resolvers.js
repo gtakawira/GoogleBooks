2f92ad358055907ac1d9364e3c56987862d81543
blacxcellence <tgtakawira@gmail.com>
2021-12-07 23:18:48 -0600

    changes made and saved

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,69 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('book');
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Invalid Credentials");
+      }
+      const correctPass = await user.isCorrectPassword(password);
+
+      if (!correctPass) {
+        throw new AuthenticationError("Invalid Credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    // use context to that we know which user profile to save the book data to
+
+    addBook: async (parent, { input }, context) => {
+      // check to see if there is a user logged in
+      // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+      
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: input} },
+          {
+            new: true,
+            runValidators: true,
+          },
+        );
+      }
+      // If user attempts to execute this mutation and isn't logged in, throw an error
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    removeBook: async (parent, {bookId}, context)=>{
+      // Make it so a logged in user can only remove a book from their own profile  
+      if(context.user){
+          return User.findOneAndUpdate(
+              {_id: context.user._id},
+              {$pull: {savedBooks: {bookId: bookId}}},
+              {new: true},
+          )
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    }
+  },
+};
+
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]!
+  }
+  type Book {
+    bookId: String!
+    authors: [String]!
+    description: String
+    image: String
+    link: String
+    title: String
+  }
+  # want to be able to add book and update book list so we need an input
+  input savedBookInput {
+    bookId: String!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+    title: String!
+  }
+  # Set up an Auth type to handle returning data from a profile creating or user login
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    # Set up mutations to handle creating a profile or logging into a profile and return Auth type
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    #setup mutations to add book to savedbooks arr and one to remove from arr
+    addBook(input: savedBookInput!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
